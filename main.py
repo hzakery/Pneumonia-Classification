fe0d025cea897ce79ed44631316e2557a42f757d
Hassan_Zakery <126134324+Tenacityv@users.noreply.github.com>
2024-01-04 11:55:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
 model = load_model('pneumonia_classifier.h5')
 
 # load class names
-with open('./model/labels.txt', 'r') as f:
+with open('labels.txt', 'r') as f:
     class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
     f.close()
 
